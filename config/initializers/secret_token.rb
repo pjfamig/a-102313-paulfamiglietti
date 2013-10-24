5524cd8cb5f09143afd49e722c91b82da05b9f99
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-10-23 20:16:41 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A102313Paulfamiglietti::Application.config.secret_key_base = 'c75ee6e078a43c1df90960d9a82b85e954ef2d29ec84bea558b8a1c1b41ac17b060bfa5285749f38da6c79a92dc117e12ebbce93d6f6bcb6372c2806d8efa72c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+A102313Paulfamiglietti::Application.config.session_store :cookie_store, key: '_a-102313-paulfamiglietti_session'
