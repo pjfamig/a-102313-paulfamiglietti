3204de9f01156c2d0a4dff84ab62a1d1f6823d26
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-10-26 16:27:07 -0700

    Updated about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,39 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Paul Famiglietti" }
+
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Paul Famiglietti') }
-    it { should have_title(full_title('')) }
+    
+    let(:heading) { 'Paul Famiglietti'}
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages" 
     it { should_not have_title('| Home') }
   end
   
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About Me') }
-    it { should have_title(full_title('')) }
+    
+    let(:heading) { 'About Me' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
   end
   
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact Me') }
-    it { should have_title(full_title('')) }
+    
+    let(:heading) { 'Contact Me' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
   end
 end
