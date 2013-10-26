d62d39ee2b81f079e3251faa14299dc4048cc78e
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-10-26 12:42:09 -0700

    Added title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Paul Famiglietti" }
 
   describe "Home page" do
     it "should have the content 'Paul Famiglietti'" do
       visit '/static_pages/home'
       expect(page).to have_content('Paul Famiglietti')
     end
+    
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Paul Famiglietti | Home")
+      expect(page).to have_title("Paul Famiglietti")
     end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+    
   end
   
   describe "About page" do
@@ -20,7 +28,7 @@ describe "Static pages" do
     end
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Paul Famiglietti | About Me")
+      expect(page).to have_title("#{base_title} | About Me")
     end
   end
   
@@ -31,7 +39,7 @@ describe "Static pages" do
     end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Paul Famiglietti | Contact Me")
+      expect(page).to have_title("#{base_title} | Contact Me")
     end
   end
 end
