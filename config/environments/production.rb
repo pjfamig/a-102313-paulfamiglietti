ebf37aae41a6c494da387ddcab4d681ea47dbcf8
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-11-22 16:49:31 -0800

    updated env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ A102313Paulfamiglietti::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => "paul@paulfamiglietti.com",
-    :password             => "lIveacROssWIrE",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => :plain,
     :enable_starttls_auto => true,
     :openssl_verify_mode => 'none'
